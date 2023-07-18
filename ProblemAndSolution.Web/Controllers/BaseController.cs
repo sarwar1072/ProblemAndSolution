0f8368e989ae509064d0b8d83b2991bfbe75fe25
sarwar <sarwar.cse.diu@gmail.com>
2023-07-18 18:45:19 +0600

    add register controller

---
+++ ProblemAndSolution.Web/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Mvc;
+using ProblemAndSolution.Web.Enums;
+using ProblemAndSolution.Web.Models;
+
+namespace ProblemAndSolution.Web.Controllers
+{
+    public class BaseController<T> : Controller where T:Controller
+    {
+        protected readonly ILogger<T> _logger;
+        protected ILifetimeScope _lifetimeScope;
+        public BaseController(ILogger<T> logger, ILifetimeScope lifetimeScope)
+        {
+            _logger = logger;
+            _lifetimeScope = lifetimeScope;
+        }
+
+        protected virtual void ViewResponse(string message, ResponseTypes responseTypes)
+        {
+            TempData.Put("ResponseMessage",new ResponseModel
+            {
+                Message = message,
+                Response=responseTypes,
+            });
+
+        }
+
+    }
+}
---
+++ ProblemAndSolution.Web/Models/RegistrationConfirmationModel.cs
@@ -0,0 +1,29 @@
+﻿using ProblemAndSolution.Membership.BusinessObj;
+using ProblemAndSolution.Membership.Services;
+
+namespace ProblemAndSolution.Web.Models
+{
+	public class RegistrationConfirmationModel
+	{
+		private ISignInManagerAdapter<ApplicationUser>? _signInManagerAdapter;
+		private IUserManagerAdapter<ApplicationUser>? _userManagerAdapter;
+
+		public string? Email { get; set; }
+
+		public bool DisplayConfirmAccountLink { get; set; }
+
+		public string? EmailConfirmationUrl { get; set; }
+
+		public RegistrationConfirmationModel()
+        {
+
+        }
+
+		public RegistrationConfirmationModel(ISignInManagerAdapter<ApplicationUser> signInManagerAdapter,
+			IUserManagerAdapter<ApplicationUser> userManagerAdapter)
+		{
+			_signInManagerAdapter = signInManagerAdapter;
+			_userManagerAdapter = userManagerAdapter;
+		}
+	}
+}
